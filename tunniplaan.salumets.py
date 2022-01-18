5fad5db5e9f2d4782f38d5d462db5e41ec925fd4
Opilane TTHK <opilane@tthk.ee>
2022-01-18 11:28:21 +0200

    Add project files.

---
+++ tunniplaan.salumets.py
@@ -0,0 +1,71 @@
+from tkinter import*
+root = Tk()
+
+Label(text="0:").grid(row=0, column=1)
+Label(text="1:").grid(row=0, column=2)
+Label(text="2:").grid(row=0, column=3)
+Label(text="3:").grid(row=0, column=4)
+Label(text="4:").grid(row=0, column=5)
+Label(text="5:").grid(row=0, column=6)
+Label(text="6:").grid(row=0, column=7)
+Label(text="7:").grid(row=0, column=8)
+Label(text="8:").grid(row=0, column=9)
+Label(text="9:").grid(row=0, column=10)
+Label(text="10:").grid(row=0, column=11)
+Label(text=" ").grid(row=0, column=0)
+Label(text="Esmaspäev").grid(row=1, column=0)
+Label(text="Teisipäev ").grid(row=2, column=0)
+Label(text="Kolmapäev ").grid(row=3, column=0)
+Label(text="Neljapäev ").grid(row=4, column=0)
+Label(text="Reede ").grid(row=5, column=0)
+Label(text=" ",font="Arial,50",width=10,height=3,bg="white",relief=RIDGE).grid(row=1, column=1)
+Label(text="tugiõppe ",font="Arial,50",width=10,height=3,bg="grey",relief=RIDGE).grid(row=1, column=2)
+Label(text="Logistika teenused ",font="Arial,50",width=20,height=3,bg="green",relief=RIDGE).grid(row=1, column=3,columnspan=2)
+Label(text="Matemaatika ",font="Arial,50",width=10,height=3,bg="pink",relief=RIDGE).grid(row=1, column=5)
+Label(text="Matemaatika ",font="Arial,50",width=10,height=3,bg="pink",relief=RIDGE).grid(row=1, column=6)
+Label(text=" ",font="Arial,50",width=10,height=3,bg="white",relief=RIDGE).grid(row=1, column=7)
+Label(text="keel ja\n kirjandus ",font="Arial,50",width=10,height=3,bg="lightgreen",relief=RIDGE).grid(row=1, column=8)
+Label(text="keel ja\n kirjandus ",font="Arial,50",width=10,height=3,bg="lightgreen",relief=RIDGE).grid(row=1, column=9)
+Label(text="tugiõppe\nmatemaatika ",font="Arial,50",width=10,height=3,bg="pink",relief=RIDGE).grid(row=1, column=10)
+Label(text=" ",font="Arial,50",width=20,height=3,bg="white",relief=RIDGE).grid(row=2, column=1)
+Label(text="tugiõppe ",font="Arial,50",width=10,height=3,bg="purple",relief=RIDGE).grid(row=2, column=2)
+Label(text="Programmeerimise alused ",font="Arial,50",width=30,height=3,bg="lightblue",relief=RIDGE).grid(row=2, column=4,columnspan=1)
+Label(text=" ").grid(row=2, column=5)
+Label(text="Füüsika ",font="Arial,50",width=20,height=3,bg="pink",relief=RIDGE).grid(row=2, column=6,columnspan=2)
+Label(text=" ").grid(row=3, column=1)
+Label(text="tugiõppe ",font="Arial,50",width=10,height=3,bg="purple",relief=RIDGE).grid(row=3, column=2)
+Label(text="kunstiained ",font="Arial,50",width=20,height=3,bg="purple",relief=RIDGE).grid(row=3, column=3,columnspan=2)
+Label(text=" ").grid(row=3, column=5)
+Label(text="Kehaline kasvatus ",font="Arial,50",width=20,height=3,bg="purple",relief=RIDGE).grid(row=3, column=6,columnspan=2)
+Label(text=" ").grid(row=3, column=8)
+Label(text=" ").grid(row=3, column=9)
+Label(text=" ").grid(row=3, column=10)
+Label(text="Logistika\nteenused ",font="Arial,50",width=20,height=3,bg="green",relief=RIDGE).grid(row=4, column=1,columnspan=2)
+Label(text=" ").grid(row=4, column=3)
+Label(text="Programmerimise\nalused ",font="Arial,50",width=20,height=3,bg="lightblue",relief=RIDGE).grid(row=4, column=4,columnspan=2)
+Label(text="Merkulova ",font="Arial,50",width=20,height=3,bg="red",relief=RIDGE).grid(row=4, column=6,columnspan=2)
+Label(text="Eesti keel ",font="Arial,50",width=20,height=3,bg="grey",relief=RIDGE).grid(row=4, column=8,columnspan=2)
+Label(text=" ").grid(row=4, column=9)
+Label(text=" ").grid(row=5, column=0)
+Label(text="Merkulova ",font="Arial,50",width=40,height=3,bg="red",relief=RIDGE).grid(row=5, column=1,columnspan=2)
+Label(text="Programmerimise alused(eesti keeles) ",font="Arial,50",width=65,height=3,bg="lightblue",relief=RIDGE).grid(row=5, column=3,columnspan=7)
+Label(text="    Inglise keel  ",font="Arial,50",width=20,height=3,bg="lightgreen",relief=RIDGE).grid(row=5, column=9,columnspan=2)
+Label(text=" ").grid(row=5, column=10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
+
+
+
+
